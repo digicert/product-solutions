c04be23e69d1572be366bced9dcd14056419daa6
DigiCertMike <163835982+DigiCertMike@users.noreply.github.com>
2025-04-16 07:51:42 +0100

    Delete api-calls/fortiweb/Create upload_pfx_to_fortiweb.sh

--- api-calls/fortiweb/Create upload_pfx_to_fortiweb.sh
+++
@@ -1,8 +0,0 @@
-# Upload PCKS12 certificate to Fortiweb using API
-curl -k --location -g --request POST 'https://ec2-18-218-166-16.us-east-2.compute.amazonaws.com:8443/api/v2.0/system/certificate.local.import_certificate' \
---header 'Authorization: REMOVED_SECRET' \
---header 'Accept: application/json' \
---header 'Content-Type: multipart/form-data' \
---form 'certificateWithKeyFile=@//Users/michaelrudloff/Desktop/certificate.pfx' \
---form 'password="P4ssw0rd!"' \
---form 'type=PKCS12Certificate'
