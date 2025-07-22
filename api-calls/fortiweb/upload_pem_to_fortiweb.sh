0d3e7f865b09b866755fb193bd62cb97175a0580
DigiCertMike <michael.rudloff@digicert.com>
2025-07-22 12:11:08 +0100

    .

--- api-calls/fortiweb/upload_pem_to_fortiweb.sh
+++
@@ -1,8 +0,0 @@
-# Upload PEM Certificate to Fortiweb using API
-curl -k --location -g --request POST 'https://ec2-18-218-166-16.us-east-2.compute.amazonaws.com:8443/api/v2.0/system/certificate.local.import_certificate' \
---header 'Authorization: eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJQbGFudCVzYXBzdXAyISIsInZkb20iOiJyb290In0=' \
---header 'Accept: application/json' \
---header 'Content-Type: multipart/form-data' \
---form 'certificateFile=@//Users/michaelrudloff/Desktop/cert.pem' \
---form 'keyFile=@//Users/michaelrudloff/Desktop/key.pem' \
---form 'type=certificate'
