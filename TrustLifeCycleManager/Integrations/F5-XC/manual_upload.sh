022b63cb652a3a42e0b15f4be8a6ecafa6e041eb
DigiCertMike <michael.rudloff@digicert.com>
2025-09-09 15:32:54 +0100

    .

---
+++ TrustLifeCycleManager/Integrations/F5-XC/manual_upload.sh
@@ -0,0 +1,74 @@
+curl -X PUT \
+  https://digicert.console.ves.volterra.io/api/config/namespaces/default/certificates/tls \
+  -H "Authorization: APIToken REMOVED_SECRET" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "metadata": {
+      "name": "tls",
+      "namespace": "default"
+    },
+    "spec": {
+      "certificate_url": "string:///'"$(cat 3days.com.crt | base64 -w 0)"'",
+      "private_key": {
+        "blindfold_secret_info": {
+          "decryption_provider": "ves-io-default-blindfold",
+          "store_provider": "ves-io-default-secret",
+          "url": "string:///'"$(cat 3days.com.key | base64 -w 0)"'"
+        }
+      }
+    }
+  }'
+
+
+
+## Replace: 
+
+curl -X PUT \
+  https://digicert.console.ves.volterra.io/api/config/namespaces/default/certificates/tls \
+  -H "Authorization: APIToken REMOVED_SECRET" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "metadata": {
+      "name": "tls",
+      "namespace": "default"
+    },
+    "spec": {
+      "certificate_url": "string:///'"$(cat 365days.com.crt | base64 -w 0)"'",
+      "private_key": {
+        "clear_secret_info": {
+          "url": "string:///'"$(cat 365days.com.key | base64 -w 0)"'"
+        }
+      }
+    }
+  }'
+
+
+
+# First delete the existing certificate
+curl -X DELETE \
+  https://digicert.console.ves.volterra.io/api/config/namespaces/default/certificates/tls \
+  -H "Authorization: APIToken REMOVED_SECRET" \
+  -H "Content-Type: application/json"
+
+# Wait a moment for deletion to complete
+sleep 2
+
+# Then create the new certificate
+curl -X POST \
+  https://digicert.console.ves.volterra.io/api/config/namespaces/default/certificates \
+  -H "Authorization: APIToken REMOVED_SECRET" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "metadata": {
+      "name": "tls",
+      "namespace": "default"
+    },
+    "spec": {
+      "certificate_url": "string:///'"$(cat 365days.com.crt | base64 -w 0)"'",
+      "private_key": {
+        "clear_secret_info": {
+          "url": "string:///'"$(cat 365days.com.key | base64 -w 0)"'"
+        }
+      }
+    }
+  }'
